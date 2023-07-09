1b0094bd12ce86f344dad41fdb53e3119af76b36
Chris Butera <cbutera@pch.com>
2023-07-09 16:37:33 -0400

    starting nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <nav class="flex justify-between">
+    <h2>Huntington Village Cooperative</h2>
+    <ul class="flex flex-row">
+      <li>About</li>
+      <li>Photo Gallery</li>
+      <li>Management</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+ul li {
+  padding: 0 1rem;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/components/**/*.{vue, js}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
